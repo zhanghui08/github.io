97774103ba4c2df45620c148f12f9bc7a503d391
往事随风 <499393751@qq.com>
2022-08-11 20:51:52 +0800

    修改配置

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vitepress/dist
@@ -15,12 +15,13 @@ cd docs/.vitepress/dist
 git init
 git add -A
 git commit -m 'deploy'
+git branch -M main
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 
-git push -f git@github.com:zhanghui08/myBlog.git master:gh-pages
+git push -f git@github.com:zhanghui08/myBlog.git main
 
 cd -
